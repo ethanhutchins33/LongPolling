94487543060b89a2d8c118d3ba6ca6dee9e685d6
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-01-14 01:09:53 +0000

    write basic api to return random number

--- API/Controllers/PollController.cs
+++ API/Controllers/PollController.cs
@@ -1,16 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 
+[ApiController]
+[Route("api")]
 public class PollController : ControllerBase
 {
-    public PollController()
-    {
+    private Random rand = new Random();
 
-    }
-
+    [HttpGet]
+    [Route("poll")]
     public ActionResult<int> Poll()
     {
-        var rand = new Random();
-        return Ok(rand.Next());
+        var next = rand.Next(1, 1000);
+        Console.WriteLine($"POLLED! {next}");
+        return Ok(new { number = next });
     }
-
 }
